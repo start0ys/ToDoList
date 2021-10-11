d31a419a8550f9e69890dffedaee514b55743921
시작 <78341551+start0ys@users.noreply.github.com>
2021-10-11 16:24:27 +0900

    캘린더 완성

---
+++ TDL/calendar.js
@@ -0,0 +1,92 @@
+var id = 0;
+var add = 0;
+var del = 0;
+let schedules = [];
+if(add == 1){
+    console.log('h');
+    $("#+").attr('class','activeBtn');
+}
+function plus() { add = 1; del = 0; $("#plusBtn").attr('class','activeBtn'); $("#minusBtn").attr('class','nomalBtn');}
+function minus() { del = 1; add = 0; $("#minusBtn").attr('class','activeBtn'); $("#plusBtn").attr('class','nomalBtn');}
+function uuid() {
+    function s4() {
+        return ((1 + Math.random()) * 0x10000 | 0).toString(16).substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
+function setschedules(){
+    localStorage.setItem('schedule',JSON.stringify(schedules)); //string형식으로 변환해서 local storage에 저장하기
+}
+function showschedules(data){
+    schedules.push(data);
+    setschedules();
+}
+
+function createCalendar(schedule){
+  $('.fc-event-time').remove(); 
+  // calendar element 취득
+  var calendarEl = $('#calendar')[0];
+  // full-calendar 생성하기
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    //with: '30%', 
+    height: '600px', // calendar 높이 설정
+    expandRows: true, // 화면에 맞게 높이 재설정
+    slotMinTime: '00:00', // Day 캘린더에서 시작 시간
+    slotMaxTime: '24:00', // Day 캘린더에서 종료 시간
+    // 해더에 표시할 툴바
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+    },
+    initialView: 'dayGridMonth', // 초기 로드 될때 보이는 캘린더 화면(기본 설정: 달)
+    // navLinks: true, // 날짜를 선택하면 Day 캘린더나 Week 캘린더로 링크
+    editable: true, // 수정 가능?
+    selectable: true, // 달력 일자 드래그 설정가능
+    nowIndicator: true, // 현재 시간 마크
+    dayMaxEvents: true, // 이벤트가 오버되면 높이 제한 (+ 몇 개식으로 표현)
+    locale: 'ko', // 한국어 설정
+    eventClick:function(event) {
+      if(del == 1) { var confirmDel = confirm(event.event.title + " 을 제거 하시겠습니까?"); }
+      if(confirmDel){
+        calendar.getEventById(event.event.id).remove();
+        const clearSchedules = schedules.filter(data =>{
+            return data.id !== event.event.id;
+        }); 
+        schedules = clearSchedules; 
+        setschedules();
+        $("#minusBtn").attr('class','nomalBtn');
+      }
+      del = 0;
+      $('.fc-event-time').remove();
+    },
+    events: schedule,
+    select: function(arg) { // 캘린더에서 드래그로 이벤트를 생성할 수 있다.
+      if(add == 1) {var title = prompt('일정을 입력해주세요');}
+      if (title) {
+        const scheduleObj = {
+            id: uuid(),
+            title: title,
+            start: arg.start,
+            end: arg.end,
+            allDay: arg.allDay
+        };
+        schedules.push(scheduleObj);
+        setschedules();
+        $("#plusBtn").attr('class','nomalBtn');
+        calendar.addEvent(scheduleObj)
+        $('.fc-event-time').remove();
+      }
+      add = 0;
+      calendar.unselect()
+    }
+    
+  });
+  // 캘린더 랜더링
+  calendar.render();
+}
+
+const loadedschedules = localStorage.getItem('schedule'); //localstorange에 저장된 값을 불러오기
+const parsedschedules = JSON.parse(loadedschedules); //string방식으로 저장한걸 가져와서 다시 객체형식으로 변환해야한다.
+parsedschedules.forEach(data => showschedules(data));
+createCalendar(parsedschedules);
---
+++ TDL/clock.js
@@ -0,0 +1,55 @@
+var clock1 = document.getElementById("clock");
+var date1 = document.getElementById("date");
+
+setInterval(function () {
+    var now = new Date();
+    var clock = now.toString();
+    var a = [];
+    var b = clock.indexOf("G");
+    var e = clock.indexOf(" ");
+    var d = clock.indexOf(" " ,e+1);
+    var c = clock.indexOf(" " ,d+1);
+    var f = clock.indexOf(" " ,c+1);
+  
+
+    var year=[];
+    for(var i = c; i < f; i++){ year += clock[i]; }
+    var date=[];
+    for (var i = d; i < c; i++) { date += clock[i]; }
+    var month = [];
+    for (var i = e + 1; i < d; i++) { month += clock[i]; }
+    var day = [];
+    for (var i = 0; i < e; i++) { day += clock[i]; }
+   
+
+    if (month == 'Jan') { month = '1월'; }
+    else if (month == 'Feb') { month = '2월'; }
+    else if (month == 'Mar') { month = '3월'; } 
+    else if (month == 'Apr') { month = '4월'; } 
+    else if (month == 'May') { month = '5월'; } 
+    else if (month == 'Jun') { month = '6월'; } 
+    else if (month == 'Jul') { month = '7월'; } 
+    else if (month == 'Aug') { month = '8월'; } 
+    else if (month == 'Sep') { month = '9월'; } 
+    else if (month == 'Oct') { month = '10월';} 
+    else if (month == 'Nov') { month = '11월';} 
+    else if (month == 'Dec') { month = '12월';}
+         
+    
+    if (day == 'Mon') { day = '월요일'; }
+    else if (day == 'Tue') { day = '화요일'; }
+    else if (day == 'Wed') { day = '수요일'; }
+    else if (day == 'Thu') { day = '목요일'; }
+    else if (day == 'Fri') { day = '금요일'; }
+    else if (day == 'Sat') { day = '토요일'; }
+    else if (day == 'Sun') { day = '일요일'; }
+
+    
+    for (var i = f; i < b; i++) {
+        a += clock[i];
+    }
+
+    clock1.innerHTML =  a ;
+    date1.innerHTML =  year + "년" + " " + month+date+"일" + " " + day ;
+
+});
