3a3dabbad53b5d3c3d770e0daf09c3ca9ba7b2bf
시작 <78341551+start0ys@users.noreply.github.com>
2021-10-12 22:18:25 +0900

    모바일 터치시간 줄이기

--- TDL/calendar.js
+++ TDL/calendar.js
@@ -60,6 +60,7 @@ function createCalendar(schedule){
     selectable: true, // 달력 일자 드래그 설정가능
     nowIndicator: true, // 현재 시간 마크
     dayMaxEvents: true, // 이벤트가 오버되면 높이 제한 (+ 몇 개식으로 표현)
+    longPressDelay: 100,
     locale: 'ko', // 한국어 설정
     eventClick:function(event) {
       if(del == 1) { var confirmDel = confirm(event.event.title + " 을 제거 하시겠습니까?"); }
